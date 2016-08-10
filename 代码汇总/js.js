0e04ea4f3a32715c402ce83e3ee2c30028ed6971
heyuanlong <1023417614@qq.com>
2016-08-10 18:45:31 +0800

    sdflsf

--- 代码汇总/js.js
+++ 代码汇总/js.js
@@ -26,6 +26,11 @@ $("#upload_photo_form").css('display',"none");//隐藏添加图片
 
 
 js函数不介意传递进来多少个参数，也不在乎穿进来参数是什么数据类型,不足的伪undefined
+js判断undefined类型
+if (typeof(reValue) == "undefined") { 
+   alert("undefined"); 
+}  
+
 
 表单提交前的处理
 <input type=submit name="submit1" value="登陆" onclick="return check(this.form)">  
