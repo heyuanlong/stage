d85da678236542805e2dcdf09e9deed411a07de6
heyuanlong <1023417614@qq.com>
2018-06-11 20:53:02 +0800

    sdfsd

--- 代码汇总/js.js
+++ 代码汇总/js.js
@@ -82,3 +82,48 @@ window.parent.document.getElementById("browserframe").src="../blank.jsp";
 	xhr.send('data='+data);
 
 
+//--------------------------------------------------------------
+Converts a javascript number from scientific notation to a decimal string
+https://gist.github.com/jiggzson/b5f489af9ad931e3d186
+function scientificToDecimal(num) {
+    //if the number is in scientific notation remove it
+    if(/\d+\.?\d*e[\+\-]*\d+/i.test(num)) {
+        var zero = '0',
+            parts = String(num).toLowerCase().split('e'), //split into coeff and exponent
+            e = parts.pop(),//store the exponential part
+            l = Math.abs(e), //get the number of zeros
+            sign = e/l,
+            coeff_array = parts[0].split('.');
+        if(sign === -1) {
+            num = zero + '.' + new Array(l).join(zero) + coeff_array.join('');
+        }
+        else {
+            var dec = coeff_array[1];
+            if(dec) l = l - dec.length;
+            num = coeff_array.join('') + new Array(l+1).join(zero);
+        }
+    }
+    
+    return num;
+};
+/*
+Usage: 
+var converted = scientificToDecimal('2.594e40');
+console.log(converted);
+*/
+//--------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
